3106d5bc0c167c82f7d4f223a71ca8c3c38084cb
KennethRoger <kennethroger11@gmail.com>
2025-08-27 00:53:53 +0530

    Fixed some errors caused by inconsitent file path, missing callback in bindAsync

--- src/grpc/index.js
+++ src/grpc/index.js
@@ -1,8 +1,10 @@
+const path = require("node:path");
+
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const GenerateToken = require("./methods/GenerateToken");
 
-const PROTO_PATH = "../../proto/auth.proto";
+const PROTO_PATH = path.join(__dirname, "../../proto/auth.proto");
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true });
 
 const grpcObj = grpc.loadPackageDefinition(packageDefinition)
@@ -13,9 +15,13 @@ function startGRPC() {
   const PORT = process.env.GRPC_PORT;
 
   server.addService(auth.AuthService.service, { GenerateToken });
-  server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure());
-
-  console.log(`grpc server started on ${PORT}`);
+  server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
+    if (err) {
+      console.error("Failed to bind gRPC server:", err);
+      return;
+    }
+    console.log(`grpc server started on ${port}`);
+  });
 }
 
 module.exports = startGRPC;
